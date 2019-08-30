109d746648b2df240e7c150152e7a93acc9f5573
Danijel Tudek <21341606+danijelt@users.noreply.github.com>
2019-08-30 18:58:57 +0200

    sondepi/post-image: override config/cmdline files

--- board/sondepi/post-image.sh
+++ board/sondepi/post-image.sh
@@ -9,6 +9,9 @@ GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
 rm -rf "${GENIMAGE_TMP}"
 
+cp -f ${BOARD_DIR}/config.txt ${BINARIES_DIR}/rpi-firmware/config.txt
+cp -f ${BOARD_DIR}/cmdline.txt ${BINARIES_DIR}/rpi-firmware/cmdline.txt
+
 genimage                           \
 	--rootpath "${TARGET_DIR}"     \
 	--tmppath "${GENIMAGE_TMP}"    \
